42fc752233a4fd5ee420e09208062c91ea67ccc3
Omer Omer <omerahm69@gmail.com>
2025-01-14 19:03:44 +0000

    Changed index view

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello world')
-    if request.method == "GET":
-       return HttpResponse("This was a GET request")
-    elif request.method == "POST":
-       return HttpResponse("This was a POST request")
+    if request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+    else:
+      
+       return HttpResponse("request.method")
